c67d8e73b95ad58a1978c099fe2123cca079dd37
Jonathan Courat <jonathan.courat@Digital-Grenoble.local>
2017-12-18 11:21:22 +0100

    Ajouter la date du jour (date du serveur) #1

--- machineCafe.php
+++ machineCafe.php
@@ -1,6 +1,7 @@
 <?php
 	$listeBoissons = array("expresso", " café long", "thé");
 	$message = "En attente ...";
+	$today = date("d.m.y");        
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +12,7 @@
 <body>
 	<?php
 		echo "<h1>Coffee machine</h1>";
+		echo "<h3>Date du jour : ".$today."</h3>";
 		foreach ($listeBoissons as $key) {
 		 	echo "<h2>".$key."</h2>";
 		}
