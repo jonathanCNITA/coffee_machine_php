9409b3a6536b4dc182134b5a942eaf690ba9982e
Jonathan Courat <jonathan.courat@Digital-Grenoble.local>
2017-12-18 16:38:46 +0100

    add one hour to the original result

--- machineCafe.php
+++ machineCafe.php
@@ -1,27 +1,33 @@
 <?php
-	$listeBoissons = array("expresso", " café long", "thé");
-	$message = "En attente ...";
-	$today = date("d.m.y");
-	$montant = 0;
-	function afficherBoisson( $boissons ) {
-		foreach($boissons as $boisson) {
-			print "<h2>$boisson</h2>";
-		}
-	}
+    setlocale (LC_TIME, 'fr_FR.utf8','fra');
+
+    $listeBoissons = array("expresso", " café long", "thé");
+    $message = "En attente ...";
+    $heureLocale = date("H")+1;
+    $today = date("d.m.y  \| $heureLocale:i");
+    $montant = 0;
+
+    function afficherBoisson( $boissons ) {
+        foreach($boissons as $boisson) {
+
+
+            print "<h2>$boisson</h2>";
+        }
+    }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Coffee machine in php</title>
+    <title>Coffee machine in php</title>
 </head>
 <body>
-	<h1>Coffee machine</h1>
-	<p>Date du jour : <?= $today ?></p>
+    <h1>Coffee machine</h1>
+    <p>Date du jour : <?= $today ?></p>
 
-	<?= afficherBoisson($listeBoissons) ?>
+    <?= afficherBoisson($listeBoissons) ?>
 
-	<p><?= $message ?></p>
-	<p>Montant : <?= $montant ?> €</p>	
+    <p><?= $message ?></p>
+    <p>Montant: <?= $montant ?> €</p>
 </body>
 </html>
