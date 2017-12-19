9d9bc77a2b8db9eec5fc98a2e23e9cb148a70333
Jonathan Courat <jonathan.courat@Digital-Grenoble.local>
2017-12-19 16:08:32 +0100

    clean the test file

--- testFunctions.php
+++ testFunctions.php
@@ -31,61 +31,6 @@
 
     <h3>Test preparerBoisson café long + 0 sucre</h3>
     <?= preparerBoisson( "the", 0, $boissonsRecette ) ?>
-
     
-
-    <!-- TEST preparerExpresso -->
-    <!-- <h2 class="testTitle">TEST preparerExpresso</h2>
-
-    <h3>Test preparerExpresso + 0 sucre</h3>
-    <?= preparerExpresso( 0 ) ?>
-
-    <h3>Test preparerExpresso + 1 sucre</h3>
-    <?= preparerExpresso( 1 ) ?>
-
-    <h3>Test preparerExpresso + 3 sucres</h3>
-    <?= preparerExpresso( 3 ) ?> -->
-
-
-    <!-- TEST preparerCafeLong -->
-    <!-- <h2 class="testTitle">TEST preparerCafeLong</h2>
-
-    <h3>Test preparerCafeLong + 0 sucre</h3>
-    <?= preparerCafeLong( 0 ) ?>
-
-     <h3>Test preparerCafeLong + 1 sucre</h3>
-    <?= preparerCafeLong( 1 ) ?>
-
-    <h3>Test preparerCafeLong + 3 sucres</h3>
-    <?= preparerCafeLong( 3 ) ?> -->
-
-
-    <!-- TEST preparerThe -->
-    <!-- <h2 class="testTitle">TEST preparerThe</h2>
-
-    <h3>Test preparerThe + 0 sucre</h3>
-    <?= preparerThe( 0 ) ?>
-
-    <h3>Test preparerThe + 1 sucre</h3>
-    <?= preparerThe( 1 ) ?>
-
-    <h3>Test preparerThe + 3 sucres</h3>
-    <?= preparerThe( 3 ) ?> -->
-
-    <!-- TEST recetteBoisson -->
-    <!-- <h2 class="testTitle">TEST recetteBoisson</h2>
-
-    <h3>Test recetteBoisson expresso + 1 sucre</h3>
-    <?= preparerBoisson( "expresso", 1 ) ?>
-
-    <h3>Test recetteBoisson café long + 3 sucre</h3>
-    <?= preparerBoisson( "cafe_long", 3 ) ?>
-
-    <h3>Test recetteBoisson café long + 0 sucre</h3>
-    <?= preparerBoisson( "the", 0 ) ?> -->
-
-    
-
-
 </body>
 </html>
