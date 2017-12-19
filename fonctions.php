1d80b89c52c912c073de53bfcc0529b0dec453bf
Jonathan Courat <jonathan.courat@Digital-Grenoble.local>
2017-12-19 10:19:00 +0100

    fonction preparer dans fonctions.php #5

---
+++ fonctions.php
@@ -0,0 +1,41 @@
+<?php
+    $listeBoissons = array("expresso", " café long", "thé");
+    $message = "En attente ...";
+    $heureLocale = date("H")+1;
+    $today = date("d.m.y  \| $heureLocale:i");
+    $montant = 0;
+
+
+    function afficherBoisson( $boissons ) {
+        foreach($boissons as $boisson) {
+            print "<h2>$boisson</h2>";
+        }
+    }
+
+
+    function preparerExpresso( $nbSucres ) {
+        $recette = array("café", "eau", $nbSucres." sucre(s)");
+        foreach($recette as $ingredient) {
+            print "<li>".$ingredient."</li>";
+        }
+   
+    }
+
+
+    function preparerCafeLong( $nbSucres ) {
+        $recette = array("2 * café", "2 * eau", $nbSucres." sucre(s)");
+        foreach($recette as $ingredient) {
+            print "<li>".$ingredient."</li>";
+        }
+       
+    }
+
+
+    function preparerThe( $nbSucres ) {
+        $recette = array("1 * thé", "2 * eau", $nbSucres." sucre(s)");
+        foreach($recette as $ingredient) {
+            print "<li>".$ingredient."</li>";
+        }
+       
+    }
+?>
